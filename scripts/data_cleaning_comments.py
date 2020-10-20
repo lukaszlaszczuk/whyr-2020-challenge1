588bb67f2401f998f2b116a4ab5e61538ffef149
lukasz.laszczuk <lukasz.laszczuk@datawalk.com>
2020-10-20 13:47:43 +0200

    Revert "Merge pull request #3 from lukaszlaszczuk/FB-Lukasz"
    This reverts commit a61830f9651215b309746c9d6843e7efbc23b734, reversing
    changes made to 68143707f9c85e669f1a74bda0ab8983561e7b07.

--- scripts/data_cleaning_comments.py
+++ scripts/data_cleaning_comments.py
@@ -1,12 +1,59 @@
 import glob
+import html
+import re
 
-from utils import clean_html_df, create_comments_df, explode_df
+import pandas as pd
 
 COMMENTS_PATH = "../data/comments*"
 OUT_PATH = "../data/comments.csv"
 
 NOT_EXPLODED_COLUMNS = ["kids"]
 HTML_COLUMN = "text"
+
+
+def create_comments_df(filenames):
+    df = None
+    for file_name in filenames:
+        if df is not None:
+            try:
+                next_df = pd.read_json('../data/' + file_name, encoding="ISO-8859-1")
+                df = pd.concat([df, next_df])
+            except Exception as e:
+                print('Cannot load json ' + file_name)
+                print(e)
+
+        else:
+            try:
+                df = pd.read_json('../data/' + file_name, encoding="ISO-8859-1")
+            except Exception as e:
+                print('Cannot load json ' + file_name)
+                print(e)
+    return df.reset_index(drop=True)
+
+
+def explode_df(df, except_columns):
+    """
+
+    :param df: input DataFrame
+    :param except_columns: columns not to be exploded
+    :return: exploded DataFrame
+    """
+
+    df = df.apply(lambda x: x.explode() if x.name not in except_columns else x)
+    return df
+
+
+def clean_html_df(df, column):
+    df[column] = df[column].fillna("").apply(html.unescape)
+    df[column] = df[column].apply(clean_html_str)
+    return df
+
+
+def clean_html_str(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
 
 if __name__ == "__main__":
     files = glob.glob(COMMENTS_PATH)
@@ -19,5 +66,5 @@ if __name__ == "__main__":
     df = clean_html_df(df, HTML_COLUMN)
 
     print('Saving csv...')
-    df.to_csv(OUT_PATH, index=False)
+    df.to_csv(OUT_PATH, index = False)
     print('csv saved successfully')
